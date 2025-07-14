d74093dfdf02ad96323e96f427eaea50ace4684b
Park Jinhyeok <harunohi0808@gmail.com>
2025-07-14 21:41:40 +0900

    [pjh] feat: Inventory Domain 구조 설계
    변경된 씬 없음
    Inventory 계층구조 설계
    Slot 기능 추상화
    Slot / ItemStack 책임 분리

---
+++ Assets/02. Scripts/Inventory/01.Domain/Aggregate/Slot.cs
@@ -0,0 +1,16 @@
+public class Slot
+{
+    /*
+    public ItemStack ItemStack { get; private set; }
+    
+    public void AddItem(ItemStack itemStack)
+    {
+        ItemStack = itemStack;
+    }
+
+    public void RemoveItem()
+    {
+        ItemStack = null;
+    }
+    */
+}
---
+++ Assets/02. Scripts/Inventory/01.Domain/Inventory.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+public class Inventory
+{
+    public List<Slot> Slots { get; private set; }
+    
+    
+}
