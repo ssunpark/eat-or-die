2ce5923bdd0def1046834dc12174acd9ffb9e254
Oc9aN <59911975+Oc9aN@users.noreply.github.com>
2025-07-15 17:41:38 +0900

    [osh] feat: 도구 아이템 구조 작성

---
+++ Assets/02. Scripts/Item/ToolItem/ToolItem.cs
@@ -0,0 +1,15 @@
+﻿public class ToolItem : AItem, IUseable
+{
+    private readonly IToolAction ToolAction;
+    
+    public ToolItem(ItemData itemData, IToolAction toolAction) : base(itemData)
+    {
+        ToolAction = toolAction;
+    }
+
+    public void Use()
+    {
+        // 도구 사용
+        ToolAction.UseTool();
+    }
+}
