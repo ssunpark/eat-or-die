8ad662daac48d523b81b1fcd9b1adb61ddd7fe7e
Park Jinhyeok <harunohi0808@gmail.com>
2025-07-17 14:34:46 +0900

    [pjh] feat: QuickSlot 디렉토리, 스크립트 생성
    Scene: InventoryTestScene
    스크립트 생성하고 폴더 추가.

---
+++ Assets/02. Scripts/Quickslots/UI_Quickslot.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_Quickslot : MonoBehaviour
+{
+	public int SlotIndex;
+	public Image IconImage;
+	public TextMeshProUGUI QuantityText;
+    
+	public void Initialize(int slotIndex)
+	{
+		SlotIndex = slotIndex;
+		IconImage.gameObject.SetActive(false);
+		QuantityText.gameObject.SetActive(false);
+		InventoryManager.Instance.OnSlotUpdated[SlotIndex] += UpdateSlotUI;
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		if (eventData.button == PointerEventData.InputButton.Left)
+		{
+			InventoryManager.Instance.OnClickMouseLeft(SlotIndex);   
+		}
+		else if (eventData.button == PointerEventData.InputButton.Right)
+		{
+			InventoryManager.Instance.OnClickMouseRight(SlotIndex);
+		}
+	}
+
+	public void UpdateSlotUI()
+	{
+		ItemStack itemInSlot = InventoryManager.Instance.Inventory.SlotList[SlotIndex].ItemStack;
+		if (itemInSlot == null)
+		{
+			IconImage.gameObject.SetActive(false);
+			QuantityText.gameObject.SetActive(false);
+			return;
+		}
+        
+		IconImage.sprite = ItemManager.Instance.GetItem(itemInSlot.ID).ItemData.Icon;
+		QuantityText.text = itemInSlot.Quantity.ToString();
+		IconImage.gameObject.SetActive(true);
+		QuantityText.gameObject.SetActive(itemInSlot.Quantity > 1);
+	}	
+}
