1992272a58db6e776344c39a07f17f566f2560ed
Park Jinhyeok <harunohi0808@gmail.com>
2025-07-17 11:02:49 +0900

    [pjh] feat: 인벤토리 우클릭 이벤트 정의
    Scene작업 없음
    아이템에 우클릭 상호작용 로직 구성

--- Assets/02. Scripts/Pointer/HandEntity.cs
+++ Assets/02. Scripts/Pointer/HandEntity.cs
@@ -13,6 +13,16 @@ public class HandEntity : BehaviourSingleton<HandEntity>
         ItemStack = itemStack;
         OnItemPickedUp?.Invoke();
     }
+
+    public bool TryAddItem(ItemStack itemStack)
+    {
+        if (itemStack.ID != ItemStack.ID) return false;
+
+        if (!ItemStack.TryAdd(itemStack.Quantity)) return false;
+        
+        OnItemPickedUp?.Invoke();
+        return true;
+    }
     
     public void DropItem()
     {
