b1ba6eb94702546b7a47ed2567235e649fbd8d67
OhSiHyeon <osh000308@gmail.com>
2025-07-20 11:56:58 +0900

    [osh] fix: 병합 실수 해결

---
+++ Assets/02. Scripts/Item/ItemType/1. UseToItem/UseToActions/UseToAction_Seed.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class UseToAction_Seed : IUseToAction
+{
+    private readonly int _seedID;
+
+    public UseToAction_Seed(int seedID)
+    {
+        _seedID = seedID;
+    }
+    
+    public void UseTool(GameObject target)
+    {
+        if (target.TryGetComponent(out SeedGround seedGround))
+        {
+            // 씨앗 심기
+            seedGround.Plant(_seedID);
+        }
+    }
+}
