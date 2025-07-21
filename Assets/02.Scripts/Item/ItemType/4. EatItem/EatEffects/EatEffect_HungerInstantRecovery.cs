c7fc87d074cb14839a883dfe13c9919e48f9485d
OhSiHyeon <osh000308@gmail.com>
2025-07-21 22:00:22 +0900

    [osh] feat: 음식 아이템 효과 생성 로직 수정 및 효과 클래스 추가

---
+++ Assets/02.Scripts/Item/ItemType/4. EatItem/EatEffects/EatEffect_HungerInstantRecovery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+// 사용 아이템 효과: 배고픔이 Value 증가 합니다.
+public class EatEffect_HungerInstantRecovery : IEatItemEffect
+{
+    private readonly float _value;
+
+    public EatEffect_HungerInstantRecovery(float value)
+    {
+        _value = value;
+    }
+
+    public void UseEffect()
+    {
+        // 매개 변수로 받은 특정 타겟에 대해 효과 적용
+        Debug.Log($"배고픔이 {_value} 증가 합니다.");
+    }
+}
--- Assets/02.Scripts/Item/ItemType/4. EatItem/EatItem.cs
+++ Assets/02.Scripts/Item/ItemType/4. EatItem/EatItem.cs
@@ -1,13 +1,47 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+public struct EatItemEffectData
+{
+    public readonly EUseItemEffectType Type;
+    public readonly float Value;
+    public readonly float Duration;
+
+    public EatItemEffectData(EUseItemEffectType type, float value, float duration)
+    {
+        Type = type;
+        Value = value;
+        Duration = duration;
+    }
+}
+
 public class EatItem : AItem, IEatable, IUsable
 {
     private readonly List<IEatItemEffect> _effects;
 
-    public EatItem(ItemData itemData, List<IEatItemEffect> effects) : base(itemData)
+    public EatItem(ItemData itemData, List<EatItemEffectData> effectDataList) : base(itemData)
     {
-        _effects = new List<IEatItemEffect>(effects);
+        _effects = new List<IEatItemEffect>();
+        foreach (var effectData in effectDataList)
+        {
+            var effect = CreateEatItemEffect(effectData.Type, effectData.Value,  effectData.Duration);
+            _effects.Add(effect);
+        }
+    }
+    
+    private IEatItemEffect CreateEatItemEffect(EUseItemEffectType type, float value, float duration)
+    {
+        return type switch
+        {
+            EUseItemEffectType.None => null,
+            EUseItemEffectType.HungerInstantRecovery => new EatEffect_HungerInstantRecovery(value),
+            EUseItemEffectType.HungerTimeRecovery => new EatEffect_HungerTimeRecovery(value, duration),
+            EUseItemEffectType.HungerConsumeReduction => new EatEffect_HungerConsumeReduction(value, duration),
+            EUseItemEffectType.MaxHunger => new EatEffect_MaxHunger(value, duration),
+            EUseItemEffectType.ManaTimeRecovery => new EatEffect_ManaTimeRecovery(value, duration),
+            EUseItemEffectType.MaxMana => new EatEffect_MaxMana(value, duration),
+            _ => null
+        };
     }
 
     public void Eat()
