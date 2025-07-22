61bc994474ea590c2715250673b6d8449f9023e0
Park Jinhyeok <harunohi0808@gmail.com>
2025-07-22 22:02:51 +0900

    [pjh] feat: Monster Spawner 추가
    Scene: EnemyRealTestScene
    
    몬스터가 생성이 돼요!

--- Assets/02.Scripts/Enemy/State/EnemyIdleState.cs
+++ Assets/02.Scripts/Enemy/State/EnemyIdleState.cs
@@ -1,9 +1,12 @@
-﻿public class EnemyIdleState : IEnemyState<EnemyStateMachine>
+﻿using UnityEngine;
+
+public class EnemyIdleState : IEnemyState<EnemyStateMachine>
 {
     public bool IsInterruptable => true;
 
     public void Enter(EnemyStateMachine stateMachine)
     {
+        Debug.Log("Entering Idle state");
     }
 
     public void Update(EnemyStateMachine stateMachine, float deltaTime)
