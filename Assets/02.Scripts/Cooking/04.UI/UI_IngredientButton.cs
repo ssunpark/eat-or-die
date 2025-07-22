3f54233ca2e0d8f6d77f406782296b7a1b889672
Suhyeon Park <suhyeon12341@gmail.com>
2025-07-22 16:14:47 +0900

    [psh] feat: 레시피 패널 아이콘 이미지 연동
    - 레시피 패널의 재료 리스트, 레시피 리스트에 들어가는 아이콘 이미지를 연동하였습니다.

--- Assets/02.Scripts/Cooking/04.UI/UI_IngredientButton.cs
+++ Assets/02.Scripts/Cooking/04.UI/UI_IngredientButton.cs
@@ -5,22 +5,38 @@ using UnityEngine.UI;
 // 수현
 public class UI_IngredientButton : MonoBehaviour
 {
-    public TextMeshProUGUI IngredientNameTextUI;
+    // public TextMeshProUGUI IngredientNameTextUI;
+    public Image IconImage;
     public Button  IngredientButtonUI;
-    private IngredientCSVData _data;
     
+    private IngredientCSVData _data;
     public int IngredientID { get; private set; }
 
     private void Start()
     {
         IngredientButtonUI.onClick.AddListener(OnClickButton);
+        // IconImage.gameObject.SetActive(false);
     }
     
     public void Refresh(IngredientCSVData data)
     {
         _data = data;
-        IngredientNameTextUI.text = _data.Name;
+        // IngredientNameTextUI.text = _data.Name;
         IngredientID = _data.ID;
+        
+        AItem item = ItemManager.Instance.GetItem(_data.ID);
+        if (item != null)
+        {
+            IconImage.sprite = item.ItemData.Icon;
+            IconImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            {
+                Debug.LogWarning($"[UI_IngredientButton] 아이템 데이터 없음 - ID: {_data.ID}");
+                IconImage.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void OnClickButton()
--- Assets/02.Scripts/Cooking/04.UI/UI_RecipeButton.cs
+++ Assets/02.Scripts/Cooking/04.UI/UI_RecipeButton.cs
@@ -1,13 +1,30 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 // 수현
 public class UI_RecipeButton : MonoBehaviour
 {
-    public TextMeshProUGUI RecipeNameTextUI;
+    // public TextMeshProUGUI RecipeNameTextUI;
+    public Image IconImage;
     private RecipeCSVData _data;
     public void Refresh(RecipeCSVData Data)
     {
         _data = Data;
-        RecipeNameTextUI.text = Data.Name;
+        // RecipeNameTextUI.text = Data.Name;
+        
+        AItem item = ItemManager.Instance.GetItem(_data.ResultID);
+        if (item != null)
+        {
+            IconImage.sprite = item.ItemData.Icon;
+            IconImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            {
+                Debug.LogWarning($"[UI_IngredientButton] 아이템 데이터 없음 - ID: {_data.ID}");
+                IconImage.gameObject.SetActive(false);
+            }
+        }
     }
 }
