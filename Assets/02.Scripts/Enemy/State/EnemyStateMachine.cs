d4f23fa9a399259d9e686c0061efb05890b85345
Park Jinhyeok <harunohi0808@gmail.com>
2025-07-22 15:55:20 +0900

    [pjh] feat: Define EnemyStateMachine
    StateMachine이 네트워크 상태에서 정상적으로 동기화 되도록 로직을 재구성

--- Assets/02.Scripts/Enemy/State/EnemyStateMachine.cs
+++ Assets/02.Scripts/Enemy/State/EnemyStateMachine.cs
@@ -6,7 +6,8 @@ using UnityEngine.AI;
 public class EnemyStateMachine : NetworkBehaviour
 {
 	// private EnemyStat _stat;
-	
+	private ChangeDetector _changeDetector;
+
 	private Animator _animator;
 	public Animator Animator => _animator;
 	
@@ -16,11 +17,14 @@ public class EnemyStateMachine : NetworkBehaviour
 	private NavMeshAgent _navMeshAgent;
 	public NavMeshAgent NavMeshAgent => _navMeshAgent;
 	
+	[Networked] private EEnemyState NetworkedState { get; set; }
+	
 	private IEnemyState<EnemyStateMachine> _currentState;
 	private Dictionary<EEnemyState, IEnemyState<EnemyStateMachine>> _stateDictionary;
 	
 	public override void Spawned()
 	{
+		_changeDetector = GetChangeDetector(ChangeDetector.Source.SimulationState);
 		_animator = GetComponent<Animator>();
 		_characterController = GetComponent<CharacterController>();
 		_navMeshAgent = GetComponent<NavMeshAgent>();
@@ -38,16 +42,46 @@ public class EnemyStateMachine : NetworkBehaviour
 		_currentState = _stateDictionary[EEnemyState.Idle];
 	}
 	
-	private void Update()
+	public override void FixedUpdateNetwork()
 	{
-		_currentState?.Update(this, Time.deltaTime);
+		if (Object.HasStateAuthority)
+		{
+			_currentState?.Update(this, Time.deltaTime);
+		}
+		else
+		{
+			foreach (string change in _changeDetector.DetectChanges(this))
+			{
+				if (change == nameof(NetworkedState))
+				{
+					_currentState?.Exit(this);
+					_currentState = _stateDictionary[NetworkedState];
+					_currentState?.Enter(this);
+				}
+			}
+		}
 	}
-	
+
 	public void RequestStateChange(EEnemyState newState)
 	{
+		if (Object.HasStateAuthority)
+		{
+			SetState(newState);
+		}
+		else
+		{
+			RPC_RequestStateChange(newState);
+		}
 	}
 	
-	public void EventStateChange(EEnemyState newState)
+	[Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+	private void RPC_RequestStateChange(EEnemyState newState)
 	{
+		SetState(newState);
+	}
+
+	private void SetState(EEnemyState newState)
+	{
+		NetworkedState = newState;
 	}
 }
