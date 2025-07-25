dccd1f7411f14fbf952d7fe8ce144a6397071d22
Suhyeon Park <suhyeon12341@gmail.com>
2025-07-25 17:15:20 +0900

    [psh] feat: CSV 기반 NPC 게임 데이터 도메인 모델 설계 및 CSV 로드 완료
    - Npc, NpcItem, NpcDialogue 도메인을 설계하고 관련 CSV 데이터를 읽어오는 작업을 진행했습니다.

---
+++ Assets/02.Scripts/NPC/03.Manager/NpcDataManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDataManager : BehaviourSingleton<NpcDataManager>
+{
+    private const string NPC_CSV_PATH = "/NpcCSV/Npc.csv";
+    private const string NPCITEM_CSV_PATH = "/NpcCSV/NpcItem.csv";
+    private const string NPCDIALOGUE_CSV_PATH = "/NpcCSV/NpcDialogue.csv";
+    
+    public List<Npc> NpcList { get; private set; }
+    public List<NpcItem> NpcItemList { get; private set; }
+    public List<NpcDialogue> NpcDialogueList { get; private set; }
+    public event Action OnDataLoaded;
+
+    private void Start()
+    {
+        InitNpcData();
+        InitNpcItemData();
+        InitNpcDialogueData();
+    }
+
+    private void InitNpcData()
+    {
+        NpcList = CSVLoader<Npc>.LoadCSV(Application.streamingAssetsPath + NPC_CSV_PATH);
+
+        Debug.Log($"로드 완료 - NpcList: {NpcList.Count}, ");
+        OnDataLoaded?.Invoke();
+    }
+    
+    private void InitNpcItemData()
+    {
+        NpcItemList = CSVLoader<NpcItem>.LoadCSV(Application.streamingAssetsPath + NPCITEM_CSV_PATH);
+
+        Debug.Log($"로드 완료 - NpcItemList: {NpcItemList.Count}, ");
+        OnDataLoaded?.Invoke();
+    }
+    
+    private void InitNpcDialogueData()
+    {
+        NpcDialogueList = CSVLoader<NpcDialogue>.LoadCSV(Application.streamingAssetsPath + NPCDIALOGUE_CSV_PATH);
+
+        Debug.Log($"로드 완료 - NpcDialogueList: {NpcDialogueList.Count}, ");
+        OnDataLoaded?.Invoke();
+    }
+}
