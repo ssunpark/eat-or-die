d915f629ae49555a7e6fe99d7cac74dcc967ca76
Oc9aN <59911975+Oc9aN@users.noreply.github.com>
2025-07-25 16:11:57 +0900

    [osh] fix: 아이템 생성 시 ExtraInfo 기본값 추가

--- Assets/02.Scripts/Item/ItemManager.cs
+++ Assets/02.Scripts/Item/ItemManager.cs
@@ -108,7 +108,7 @@ public class ItemManager : NetworkBehaviour
     /// <param name="position">생성 위치</param>
     /// <param name="rotation">생성 시 각도</param>
     [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
-    public void RPC_CreateItemObject(int id, int quantity, float durability, Vector3 position, Quaternion rotation, string extraInfo)
+    public void RPC_CreateItemObject(int id, int quantity, float durability, Vector3 position, Quaternion rotation, string extraInfo = "")
     {
         if (!Runner.IsServer)
         {
