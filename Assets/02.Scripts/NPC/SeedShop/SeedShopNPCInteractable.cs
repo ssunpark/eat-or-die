20187556865875272a1087c05860713f3f660257
Suhyeon Park <suhyeon12341@gmail.com>
2025-07-25 15:55:07 +0900

    [psh] feat: UI_SeedShopPanel 인풋 조작 구현

--- Assets/02.Scripts/NPC/SeedShop/SeedShopNPCInteractable.cs
+++ Assets/02.Scripts/NPC/SeedShop/SeedShopNPCInteractable.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 // 수현
 public class SeedShopNPCInteractable : MonoBehaviour, IInteractable
 {
+    public UI_SeedShopPanel UI_SeedShopPanel;
     public GameObject SeedShopPanelUI;
     public void Interact()
     {
-        SeedShopPanelUI.SetActive(true);
+        UI_SeedShopPanel.Open();
         InputReader.playerControllerInputBlocked = true;
     }
 }
