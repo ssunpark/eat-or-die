8616835d5bdf2c7967a6ba01c18938d6d15b409c
Oc9aN <59911975+Oc9aN@users.noreply.github.com>
2025-07-25 16:11:05 +0900

    [osh] feat: 아이템 ExtraInfo 추가 생성 메소드로 수정

--- Assets/02.Scripts/Item/ItemManager.cs
+++ Assets/02.Scripts/Item/ItemManager.cs
@@ -108,7 +108,7 @@ public class ItemManager : NetworkBehaviour
     /// <param name="position">생성 위치</param>
     /// <param name="rotation">생성 시 각도</param>
     [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
-    public void RPC_CreateItemObject(int id, int quantity, float durability, Vector3 position, Quaternion rotation)
+    public void RPC_CreateItemObject(int id, int quantity, float durability, Vector3 position, Quaternion rotation, string extraInfo)
     {
         if (!Runner.IsServer)
         {
@@ -133,6 +133,7 @@ public class ItemManager : NetworkBehaviour
                 item.Quantity = quantity;
                 item.SpawnPosition = position;
                 item.Durability = durability;
+                item.ExtraInfo = extraInfo;
             });
     }
 }
