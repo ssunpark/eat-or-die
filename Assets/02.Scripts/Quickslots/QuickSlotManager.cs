2c5ebe6e917d37096225a13429abcfb15068026f
Park Jinhyeok <harunohi0808@gmail.com>
2025-07-25 18:04:00 +0900

    [pjh] chore: namespace 삭제

--- Assets/02.Scripts/Quickslots/QuickSlotManager.cs
+++ Assets/02.Scripts/Quickslots/QuickSlotManager.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using ExitGames.Client.Photon.StructWrapping;
 using UnityEngine;
 
 public class QuickSlotManager : BehaviourSingleton<QuickSlotManager>
