0c4d2f7df138224a9964d77b706c35b9dff492ef
Suhyeon Park <suhyeon12341@gmail.com>
2025-07-29 14:24:37 +0900

    [psh] fix: InventoryTest 스크립트 수정

---
+++ Assets/02.Scripts/Craft/04.UI/UI_CraftItemButton.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 수현
+public class UI_CraftItemButton : MonoBehaviour
+{
+    public Image IconImage;
+    public TextMeshProUGUI ItemName;
+
+}
---
+++ Assets/02.Scripts/Craft/04.UI/UI_CraftItemList.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+// 수현
+public class UI_CraftItemList : MonoBehaviour
+{
+    public GameObject Container;
+    public GameObject CraftItemPrefab;
+    
+}
