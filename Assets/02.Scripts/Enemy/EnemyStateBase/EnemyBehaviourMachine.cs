c17632365b87d1fd337f929be306797008fea106
Park Jinhyeok <harunohi0808@gmail.com>
2025-07-29 19:49:46 +0900

    [pjh] rename: 클래스 명칭 변경

---
+++ Assets/02.Scripts/Enemy/EnemyStateBase/EnemyBehaviourMachine.cs
@@ -0,0 +1,10 @@
+﻿using Fusion.Addons.FSM;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviourMachine : StateMachine<EnemyStateBehaviour>
+{
+    public EnemyBehaviourMachine(string name, params EnemyStateBehaviour[] states) : base(name, states)
+    {
+    }
+}
