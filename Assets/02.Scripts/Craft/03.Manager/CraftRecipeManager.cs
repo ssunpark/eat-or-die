ec71259c41ba4b43525ad4b50800021bf2c1fdbd
Suhyeon Park <suhyeon12341@gmail.com>
2025-07-29 18:03:40 +0900

    [psh] feat: 제작대 카테고리별로 생성
    - 도구, 무기, 장비 카테고리에 따라 각 탭으로 구분하여 아이템 버튼을 생성하도록 합니다.

--- Assets/02.Scripts/Craft/03.Manager/CraftRecipeManager.cs
+++ Assets/02.Scripts/Craft/03.Manager/CraftRecipeManager.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CraftRecipeManager : BehaviourSingleton<CraftRecipeManager>
 {
-    private const string CRAFTRECIPE_CSV_PATH = "/CraftCSV/CraftRecipeTest.csv";
+    private const string CRAFTRECIPE_CSV_PATH = "/CraftCSV/CraftRecipe.csv";
     
     public List<CraftRecipe> CraftRecipeList { get; private set; } = new List<CraftRecipe>();
-    public event Action OnDataLoaded;
+    // public event Action OnDataLoaded;
 
     private void Awake()
     {
@@ -19,7 +19,7 @@ public class CraftRecipeManager : BehaviourSingleton<CraftRecipeManager>
         CraftRecipeList = CSVLoader<CraftRecipe>.LoadCSV(Application.streamingAssetsPath + CRAFTRECIPE_CSV_PATH);
 
         Debug.Log($"로드 완료 - CraftRecipeCSVDataList: {CraftRecipeList.Count}, ");
-        OnDataLoaded?.Invoke();
+        // OnDataLoaded?.Invoke();
     }
 
     public List<CraftRecipe> GetAll()
--- Assets/02.Scripts/Craft/04.UI/UI_CraftItemList.cs
+++ Assets/02.Scripts/Craft/04.UI/UI_CraftItemList.cs
@@ -5,43 +5,55 @@ using UnityEngine;
 // 수현
 public class UI_CraftItemList : MonoBehaviour
 {
-    public GameObject Container;
+    public GameObject Container1;
+    public GameObject Container2;
+    public GameObject Container3;
     public GameObject CraftItemPrefab;
     
     private readonly List<UI_CraftItemButton> _craftItemButtons = new();
+    private bool isInitialized = false;
 
     private void OnEnable()
     {
-        CraftRecipeManager.Instance.OnDataLoaded += CreateButtons;
+        if (!isInitialized)
+        {
+            InitButtons();
+            isInitialized = true;
+        }
+        RefreshAllButtons();
+        // CraftRecipeManager.Instance.OnDataLoaded += CreateButtons;
         InventoryManager.Instance.OnInventoryUpdated += RefreshAllButtons;
     }
 
     private void OnDisable()
     {
-        CraftRecipeManager.Instance.OnDataLoaded -= CreateButtons;
+        // CraftRecipeManager.Instance.OnDataLoaded -= CreateButtons;
         InventoryManager.Instance.OnInventoryUpdated -= RefreshAllButtons;
     }
 
-    private void CreateButtons()
+    private void InitButtons()
     {
-        ClearButtons();
+        CreateAllButtons(400000, 600000, Container1);
+        CreateAllButtons(600000, 700000, Container2);
+        CreateAllButtons(700000, 800000, Container3);
+    }
 
+    private void CreateAllButtons(int minID, int maxID, GameObject container)
+    {
         List<CraftRecipe> craftRecipes = CraftRecipeManager.Instance.GetAll();
         foreach (var craftRecipe in craftRecipes)
         {
-            Debug.Log("UI_CraftItemList.foreach문 진입");
+            if (craftRecipe.CraftResultID < minID || craftRecipe.CraftResultID >= maxID) continue;
             AItemInfo itemInfo = ItemManager.Instance.GetItem(craftRecipe.CraftResultID);
             if (itemInfo == null || itemInfo.ItemData == null) continue;
             
-            GameObject btn = Instantiate(CraftItemPrefab, Container.transform);
-            Debug.Log("버튼 생성!!!!!!!");
+            GameObject btn = Instantiate(CraftItemPrefab, container.transform);
+            Debug.Log("버튼 생성!!!!!");
             UI_CraftItemButton craftItemButtons = btn.GetComponent<UI_CraftItemButton>();
             craftItemButtons.Refresh(craftRecipe, itemInfo);
             btn.SetActive(false);
             _craftItemButtons.Add(craftItemButtons);
         }
-
-        RefreshAllButtons();
     }
 
     private void RefreshAllButtons()
@@ -52,14 +64,4 @@ public class UI_CraftItemList : MonoBehaviour
             button.gameObject.SetActive(true);
         }
     }
-
-    private void ClearButtons()
-    {
-        foreach (var buttons in _craftItemButtons)
-        {
-            buttons.gameObject.SetActive(false);
-        }
-        _craftItemButtons.Clear();
-    }
-    
 }
