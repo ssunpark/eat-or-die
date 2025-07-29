56b67c033432823a68c443636ab89f083ab51e7a
Suhyeon Park <suhyeon12341@gmail.com>
2025-07-29 12:30:41 +0900

    [psh] feat: 플레이어 <-> 제작대 오브젝트 상호작용 구현

---
+++ Assets/02.Scripts/Craft/04.UI/CraftingTableInteractable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CraftingTableInteractable : MonoBehaviour, IInteractable
+{
+    public UI_CraftingTablePanel CraftingTablePanel;
+    
+    public void Interact()
+    {
+        Debug.Log("E키 상호작용");
+        CraftingTablePanel.Open();
+        InputReader.playerControllerInputBlocked = true;
+    }
+}
---
+++ Assets/02.Scripts/Craft/04.UI/UI_CraftingTablePanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UI_CraftingTablePanel : AUI_PopupBase
+{
+    public override EPopupType Type => EPopupType.Craft;
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("esc키 누르기");
+            Close();
+            InputReader.playerControllerInputBlocked = false;
+        }
+    }
+}
