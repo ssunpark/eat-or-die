a588dea64d0623771dedb369746cf5d1d72049d0
KM <nam3856@gmail.com>
2025-07-30 17:42:00 +0900

    [nkm] feat: animator StateMachineBehaviour

---
+++ Assets/02.Scripts/Player/Animation/PlayerCommonStateBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerCommonStateBehaviour : StateMachineBehaviour
+{
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<PlayerAnimator>()?.OnAnimationFinished();
+    }
+}
