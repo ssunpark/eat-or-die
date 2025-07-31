50b4549f56955b790322abfd99ec3992747d4cca
Park Jinhyeok <harunohi0808@gmail.com>
2025-08-01 03:16:32 +0900

    [pjh] chore: Debug.Log 삭제
    버그 수정 완료해서 필요 없는 디버그 지움

--- Assets/02.Scripts/Enemy/EnemyStateBase/States/TraceState.cs
+++ Assets/02.Scripts/Enemy/EnemyStateBase/States/TraceState.cs
@@ -5,13 +5,11 @@ public class TraceState : AEnemyState
 {
     protected override bool CanEnterState()
     {
-        Debug.Log("TraceState.CanEnterState");
         return (Context.Target != null);
     }
 
     protected override void OnEnterState()
     {
-        Debug.Log("TraceState OnEnterState");
     }
 
     protected override void OnFixedUpdate()
