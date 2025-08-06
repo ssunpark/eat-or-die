003cf8f8871cb4ac7f5abdaececb2d0cdc5f1fa8
Oc9aN <59911975+Oc9aN@users.noreply.github.com>
2025-08-06 19:23:08 +0900

    [osh] chore: 폴더 구조 수정

---
+++ Assets/02.Scripts/Boss/Modules/DragonStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DragonStats
+{
+    public float MaxHP { get; private set; }
+    public float CurrentHP { get; private set; }
+
+    public DragonStats()
+    {
+        MaxHP = 100f;
+        CurrentHP = MaxHP;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        CurrentHP = Mathf.Max(CurrentHP - amount, 0f);
+    }
+
+    public void Heal(float amount)
+    {
+        CurrentHP = Mathf.Min(CurrentHP + amount, MaxHP);
+    }
+
+    public bool IsDead => CurrentHP <= 0f;
+}
