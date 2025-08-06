8ffe26ffb6ed628082982476627a9b5c6a4ac5c3
ssunpark <91607819+ssunpark@users.noreply.github.com>
2025-08-06 23:12:05 +0900

    [psh] chore: RooData 저장 구조 RnD

---
+++ Assets/02.Scripts/Multiplay/1.Domain/RoomInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+[Serializable]
+public class RoomInfo
+{
+    public HashSet<int> KnownIngredients;
+
+    public RoomInfo(HashSet<int> knownIngredients)
+    {
+        KnownIngredients = knownIngredients;
+    }
+}
---
+++ Assets/02.Scripts/Multiplay/2.Repository/RoomInfoRepository.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+public class RoomInfoRepository
+{
+    private RoomInfo _roomInfo;
+
+    public static string SaveKey = "RoomInfo";
+
+    // 방장 기준으로 저장 (추후 데베)
+    public void Save(RoomInfo roomInfo)
+    {
+        string data = JsonUtility.ToJson(roomInfo);
+
+        PlayerPrefs.SetString(SaveKey, data);
+        PlayerPrefs.Save();
+    }
+
+    public RoomInfo Load(RoomInfo roomInfo)
+    {
+        string jsonData = PlayerPrefs.GetString(SaveKey, null);
+        _roomInfo = JsonUtility.FromJson<RoomInfo>(jsonData);
+
+        return _roomInfo;
+    }
+}
