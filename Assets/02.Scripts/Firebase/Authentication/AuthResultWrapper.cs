81702dd2b6147bade59298a1a3cb67c7c4764a90
harunohi88 <harunohi0808@gmail.com>
2025-08-07 16:46:47 +0900

    [pjh] feat: Auth 관련 클래스 생성
    UI, Result 관련 클래스 생성

---
+++ Assets/02.Scripts/Firebase/Authentication/AuthResultWrapper.cs
@@ -0,0 +1,11 @@
+using Firebase.Auth;
+
+/// <summary>
+/// Firebase 인증 결과(성공 여부, 메시지, 사용자 정보)를 담는 래퍼 클래스입니다.
+/// </summary>
+public class AuthResultWrapper
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public FirebaseUser User { get; set; }
+}
---
+++ Assets/02.Scripts/Firebase/Authentication/UI_Authentication.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UI_Authentication : MonoBehaviour
+{
+    
+    
+}
