8395c14d51b179b3f4bc447f39a3045798441f33
kinggodmini <nam3856@gmail.com>
2025-08-10 00:33:00 +0900

    [nkm] chore: 주석 제거

--- Assets/02.Scripts/Player/Animation/PlayerAnimator.cs
+++ Assets/02.Scripts/Player/Animation/PlayerAnimator.cs
@@ -112,21 +112,6 @@ public class PlayerAnimator : NetworkBehaviour
     public void OnAnimationFinished()
     {
         TryInitializeController();
-        //미사용
-        //Debug.Log($"PlayerAnimator: OnAnimationFinished called on state {_fsm?.StateMachine?.ActiveState?.GetType().Name}");
-        //if (_fsm == null || _fsm.StateMachine == null)
-        //{
-        //    Debug.LogError("PlayerAnimator: FSM or StateMachine is null.");
-        //    return;
-        //}
-        //if (_fsm?.StateMachine?.ActiveState is IAnimationActionEndNotify notify)
-        //{
-        //    notify.OnAnimationFinished();
-        //}
-        //else
-        //{
-        //    Debug.LogWarning("PlayerAnimator: Current state is NOT IAnimationActionEndNotify");
-        //}
     }
 
 
