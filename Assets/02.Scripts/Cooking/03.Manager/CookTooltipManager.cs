aa4190c7d1b77f87f0c12fcf588a56b1c6bc9ccf
Suhyeon Park <suhyeon12341@gmail.com>
2025-08-14 00:50:33 +0900

    [psh] feat: 재료 툴팁 적용

--- Assets/02.Scripts/Cooking/03.Manager/CookTooltipManager.cs
+++ Assets/02.Scripts/Cooking/03.Manager/CookTooltipManager.cs
@@ -1,11 +1,74 @@
-using TMPro;
+using System.Collections;
 using UnityEngine;
 
-public class CookTooltipManager : MonoBehaviour
+public class CookTooltipManager : BehaviourSingleton<CookTooltipManager>
 {
-    public GameObject CookingHoverUI;
-    public TextMeshProUGUI RecipeNameText;
-    public TextMeshProUGUI RecipeDescriptionText;
-    public TextMeshProUGUI RecipeExtraDescriptionText;
-    
+    public CanvasGroup canvasGroup;
+    public UI_TooltipPanel tooltipPanel;
+    public float fadeTime = 0.15f;
+    private Coroutine _fadeCoroutine;
+
+    private void Awake()
+    {
+        if (canvasGroup != null)
+        {
+            canvasGroup.alpha = 0f;
+        }
+
+        if (tooltipPanel != null)
+        {
+            tooltipPanel.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (tooltipPanel != null && tooltipPanel.gameObject.activeSelf)
+        {
+            tooltipPanel.transform.position = Input.mousePosition;
+        }
+    }
+
+    public void Show(string content)
+    {
+        if (_fadeCoroutine != null)
+        {
+            StopCoroutine(_fadeCoroutine);
+        }
+
+        tooltipPanel.gameObject.SetActive(true);
+        tooltipPanel.SetText(content);
+
+        _fadeCoroutine = StartCoroutine(Fade(1f));
+    }
+
+    public void Hide()
+    {
+        if (_fadeCoroutine != null)
+        {
+            StopCoroutine(_fadeCoroutine);
+        }
+
+        _fadeCoroutine = StartCoroutine(Fade(0f));
+    }
+
+    private IEnumerator Fade(float targetAlpha)
+    {
+        var startAlpha = canvasGroup.alpha;
+        var timer = 0f;
+
+        while (timer < fadeTime)
+        {
+            timer += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, timer / fadeTime);
+            yield return null;
+        }
+
+        canvasGroup.alpha = targetAlpha;
+
+        if (targetAlpha == 0f)
+        {
+            tooltipPanel.gameObject.SetActive(false);
+        }
+    }
 }
--- Assets/02.Scripts/Cooking/04.UI/UI_RecipeButton.cs
+++ Assets/02.Scripts/Cooking/04.UI/UI_RecipeButton.cs
@@ -1,21 +1,15 @@
-using TMPro;
+using System.Text;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UI_RecipeButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // public TextMeshProUGUI RecipeNameTextUI;
     public Image IconImage;
     public Button RecipeButton;
     public Color UnlockedColor = Color.white;
     public Color lockedColor = Color.gray;
     public Sprite unknownIcon;
-
-    // public GameObject CookingHoverUI;
-    // public TextMeshProUGUI RecipeNameText;
-    // public TextMeshProUGUI RecipeDescriptionText;
-    // public TextMeshProUGUI RecipeExtraDescriptionText;
     
     private Recipe _data;
     public int RecipeID => _data.ID;
@@ -24,7 +18,6 @@ public class UI_RecipeButton : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void Refresh(Recipe Data)
     {
         _data = Data;
-        // RecipeNameTextUI.text = Data.Name;
         
         bool isKnown = RecipePanelUIManager.Instance.IsKnownRecipe(_data.ID);
         bool canMake = RecipePanelUIManager.Instance.CanMakeRecipe(_data);
@@ -75,38 +68,35 @@ public class UI_RecipeButton : MonoBehaviour, IPointerEnterHandler, IPointerExit
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // ui 활성화
         Debug.Log("OnPointerEnter");
         
-        // 데이터가 없거나, 아직 모르는 레시피라면 툴팁을 띄우지 않음
         if (_data == null || !RecipePanelUIManager.Instance.IsKnownRecipe(_data.ID))
         {
-            CookingHoverUI.gameObject.SetActive(false);
+            return;
         }
-        else
-        {
-            CookingHoverUI.gameObject.SetActive(true);
-        }
-        // 툴팁에 표시할 결과 아이템의 정보를 가져옴
+
         var itemProfile = ItemManager.Instance.GetItem(_data.ResultID);
         if (itemProfile == null)
         {
             return;
         }
 
-        RecipeNameText.text = itemProfile.ItemDefinition.Name;
-        RecipeDescriptionText.text = itemProfile.ItemDefinition.Description;
+        var sb = new StringBuilder();
 
-        var extraDescriotion = string.Join("  ", itemProfile.ItemDefinition.ExtraDescription);
-        extraDescriotion = RichTextUtil.RecolorAll(extraDescriotion, "#E44962");
-        RecipeExtraDescriptionText.text = extraDescriotion;
+        sb.Append($"<color=#7BD9B2><b>{itemProfile.ItemDefinition.Name}</b></color>\n\n");
+        sb.Append($"{itemProfile.ItemDefinition.Description}\n\n"); // 설명
+
+        var extraDescription = string.Join("  ", itemProfile.ItemDefinition.ExtraDescription);
+        extraDescription = RichTextUtil.RecolorAll(extraDescription, "#E44962");
+        sb.Append($"{extraDescription}");
+
+        CookTooltipManager.Instance.Show(sb.ToString());
     }
 
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // ui 비활성화
         Debug.Log("OnPointerExit");
-        CookingHoverUI.gameObject.SetActive(false);
+        CookTooltipManager.Instance.Hide();
     }
 }
