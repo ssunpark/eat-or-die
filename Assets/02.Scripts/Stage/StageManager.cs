7c220992696549444ae640f44be6e92c3c490a4f
harunohi88 <harunohi0808@gmail.com>
2025-08-18 09:49:41 +0900

    [pjh] feat: StageManager 생성
    Stage는 여기서 통합 관리

---
+++ Assets/02.Scripts/Stage/StageManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+
+public class StageManager : BehaviourSingleton<StageManager>
+{
+    [SerializeField] private List<Stage> _stages = new List<Stage>();
+    
+    
+}
