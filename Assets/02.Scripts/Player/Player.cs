65ce46473b81fa313433a2c97880f28bccdab7f5
KM <nam3856@gmail.com>
2025-08-21 23:18:44 +0900

    [nkm] fix: trait 초기화 저장이 안되는 이슈 해결

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -162,6 +162,8 @@ public class Player : CharacterBase, IAttackable
             {
                 Debug.LogError("Fuckyou");
             }
+
+            HookLocalTraitSavesIfOwner();
         }
         _spawnInitDone = true;
     }
@@ -589,9 +591,18 @@ public class Player : CharacterBase, IAttackable
 
     public void InstantRevive()
     {
-        Trait.ResetTraits();
+        RPC_ClearLocalTraitSaves();
+
         Stat.ClearAllModifiers();
         Revive();
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.InputAuthority, HostMode = RpcHostMode.SourceIsHostPlayer)]
+    private void RPC_ClearLocalTraitSaves()
+    {
+        Trait.ResetTraits();
+
+        GetComponent<CharacterTraitNetworkSync>()?.SyncAllTraits();
     }
 
     [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority,
@@ -601,6 +612,7 @@ public class Player : CharacterBase, IAttackable
         if (!HasStateAuthority) return;
 
         if (!IsDead) return;
+
         InstantRevive();
     }
 
@@ -612,5 +624,38 @@ public class Player : CharacterBase, IAttackable
 
         if (!IsDead) return;
         Revive();
+    }
+
+    private bool _traitSaveHooked;
+
+    private void HookLocalTraitSavesIfOwner()
+    {
+        if (_traitSaveHooked || Trait == null || !Object.HasInputAuthority) return;
+
+        Trait.OnTraitLeveledUp += (type, delta) =>
+        {
+            var t = Trait.GetTrait(type);
+            if (t != null)
+            {
+                TraitLevelStorage.SetLevel(type, t.Level);
+                TraitLevelStorage.SetExperience(type, t.CurrentExp);
+            }
+        };
+
+        Trait.OnTraitExpGained += (type, gained) =>
+        {
+            var t = Trait.GetTrait(type);
+            if (t != null)
+            {
+                TraitLevelStorage.SetExperience(type, t.CurrentExp);
+            }
+        };
+
+        Trait.OnSkillPointChanged += (type) =>
+        {
+            TraitLevelStorage.SetSkillPoint(type, Trait.GetSkillPoints(type));
+        };
+
+        _traitSaveHooked = true;
     }
 }
