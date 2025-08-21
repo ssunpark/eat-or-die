547aacd9e2adcc9d91d9b2a5c821eff801c3ba23
KM <nam3856@gmail.com>
2025-08-22 04:40:54 +0900

    Revert "temp"
    This reverts commit 4a14723881df17ff1a17db3ce95a4efbaf9cec28.

--- Assets/02.Scripts/Cooking/03.Manager/CookingManager.cs
+++ Assets/02.Scripts/Cooking/03.Manager/CookingManager.cs
@@ -185,7 +185,6 @@ public class CookingManager : NetworkBehaviourSingleton<CookingManager>
         UnifiedInventoryManager.Instance.AddItem(new ItemInstance(resultItem, quantity));
         RPC_BroadcastCookingResult(itemId);
         MasterAudio.PlaySound3DAtTransform("CookCompleted", _currentCookingPot.transform);
-        MasterAudio.PlaySound("CookSuccess");
         OnCompletedPopupStarted?.Invoke(new ItemInstance(resultItem, 1));
         OnItemAdded?.Invoke();
         
