273a6bf202c249f4d885b952f3cfc8b416ad2a8d
Suhyeon Park <suhyeon12341@gmail.com>
2025-08-22 12:06:11 +0900

    [psh] fix: 제작대 ui 수정

---
+++ Assets/02.Scripts/Craft/04.UI/UI_CraftCategoryText.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UI_CraftCategoryText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _categoryTextUI;
+
+    public void OnClickAllCategory()
+    {
+        _categoryTextUI.text = "전체";
+    }
+
+    public void OnClickToolCategory()
+    {
+        _categoryTextUI.text = "도구";
+    }
+
+    public void OnClickWeaponCategory()
+    {
+        _categoryTextUI.text = "무기";
+    }
+
+    public void OnClickEquipmentCategory()
+    {
+        _categoryTextUI.text = "장비";
+    }
+}
--- Assets/02.Scripts/Currency/03.Manager/CurrencyManager.cs
+++ Assets/02.Scripts/Currency/03.Manager/CurrencyManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CurrencyManager : BehaviourSingleton<CurrencyManager>
 {
     private Dictionary<ECurrencyType, int> _currencies = new();
-    private readonly int _gold = 1000;
+    private readonly int _gold = 2000;
     public event Action<ECurrencyType, int> OnCurrencyChanged;
 
     private void Awake()
@@ -17,37 +17,25 @@ public class CurrencyManager : BehaviourSingleton<CurrencyManager>
         });
     }
 
-    /// <summary>
-    /// 재화 딕셔너리를 초기화한다.
-    /// 예: {"Gold", 1000}
-    /// </summary>
+    // 재화 딕셔너리를 초기화한다.
     public void Initialize(Dictionary<ECurrencyType, int> initialValues)
     {
         _currencies = new Dictionary<ECurrencyType, int>(initialValues);
     }
 
-    /// <summary>
-    /// 특정 재화의 현재 보유량을 가져온다.
-    /// 없을 경우 0 반환.
-    /// </summary>
+    // 특정 재화의 현재 보유량을 가져온다. 없을 경우 0 반환.
     public int Get(ECurrencyType type)
     {
         return _currencies.TryGetValue(type, out var value) ? value : 0;
     }
-    
-    /// <summary>
-    /// 해당 재화를 충분히 가지고 있는지 확인한다.
-    /// </summary>
+
+    // 해당 재화를 충분히 가지고 있는지 확인한다.
     public bool HasEnough(ECurrencyType type, int required)
     {
         return Get(type) >= required;
     }
 
-    /// <summary>
-    /// 특정 재화를 차감한다.
-    /// 보유량이 부족할 경우 false 반환.
-    /// 성공 시 이벤트 발생.
-    /// </summary>
+    // 특정 재화를 차감한다. 보유량이 부족할 경우 false 반환. 성공 시 이벤트 발생.
     public bool TrySpend(ECurrencyType type, int amount)
     {
         if (!HasEnough(type, amount))
@@ -59,11 +47,7 @@ public class CurrencyManager : BehaviourSingleton<CurrencyManager>
         return true;
     }
 
-    /// <summary>
-    /// 특정 재화를 추가한다.
-    /// 없을 경우 자동으로 0으로 초기화 후 더함.
-    /// 이벤트 발생.
-    /// </summary>
+    // 특정 재화를 추가한다. 없을 경우 자동으로 0으로 초기화 후 더함. 이벤트 발생.
     public void Add(ECurrencyType type, int amount)
     {
         if (!_currencies.ContainsKey(type))
@@ -74,10 +58,7 @@ public class CurrencyManager : BehaviourSingleton<CurrencyManager>
         OnCurrencyChanged?.Invoke(type, _currencies[type]);
     }
 
-    /// <summary>
-    /// 현재 보유 중인 모든 재화 정보를 복사해서 반환한다.
-    /// 저장 용도 등으로 활용 가능.
-    /// </summary>
+    // 현재 보유 중인 모든 재화 정보를 복사해서 반환한다. 저장 용도 등으로 활용 가능.
     public Dictionary<ECurrencyType, int> GetAllCurrencies()
     {
         return new Dictionary<ECurrencyType, int>(_currencies);
